4535b1c9664b39e6c22f01333d0ff397f685e010
lighttobrca <quwieorpt@gmail.com>
2024-08-26 10:32:34 +0900

    20240826 React useContextでリマインダーアプリ作成。
    20240826 React useContextでリマインダーアプリ作成。
    更新しました。

---
+++ 【Udemy】React講座/11_hooks_p1/src/065_useContext_with_reducer/start/Example.js
@@ -0,0 +1,11 @@
+import Counter from "./components/Counter";
+import { CounterProvider } from "./context/CounterContext";
+const Example = () => {
+  return (
+    <CounterProvider>
+      <Counter />
+    </CounterProvider>
+  );
+}
+
+export default Example;
