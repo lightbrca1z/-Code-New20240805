5b25cfa24be20ba4d83ddf1bed7268294292ab6f
lighttobrca <quwieorpt@gmail.com>
2024-09-18 13:21:35 +0900

    20240918 - React. RestAPIのDynamicImportとNext.js入門。
    20240918 - React. RestAPIのDynamicImportとNext.js入門。
    更新。

--- 【Udemy】React講座/16_rest_api/src/070_dynamic_import/start/Example.js
+++ 【Udemy】React講座/16_rest_api/src/070_dynamic_import/start/Example.js
@@ -1,5 +1,17 @@
+import { add } from "./add";
+
 const Example = () => {
-  
+
+  const dynamicImport = async () =>{
+    const module = await import("./add");
+    console.log(module);
+  }
+  dynamicImport();
+
+  import("./add").then(module => {
+    console.log(module);
+  })
+  console.log(add(1,2))
 };
 
 export default Example;
